974338bcdd129a47d95ceca6d48b241a5693d45e
erirfan04 <scgirfan@gmail.com>
2025-08-17 20:14:07 +0530

    "Create controller class and added test method"

---
+++ src/main/java/com/example/ecommerce/controller/MyController.java
@@ -0,0 +1,14 @@
+package com.example.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ecommerce")
+public class MyController {
+    @GetMapping("greeting")
+    public String test(){
+        return " Welcome to Spring boot";
+    }
+}
